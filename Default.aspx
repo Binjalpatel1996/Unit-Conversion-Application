f11fd3ff5374897168f52ae1a5a5998905d61ea0
Binjalpatel1996 <48345310+Binjalpatel1996@users.noreply.github.com>
2019-04-05 17:30:10 -0500

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,98 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Assignment1.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="Stylesheet.css" rel="stylesheet" />
+
+    <style type="text/css">
+        .auto-style1 {
+            width: 27%;
+        }
+        .auto-style2 {
+            height: 52px;
+            width: 46px;
+        }
+        .auto-style8 {
+            margin-left: 2px;
+        }
+        .auto-style11 {
+            height: 52px;
+            width: 329px;
+        }
+        .auto-style13 {
+            width: 329px;
+            height: 66px;
+        }
+        .auto-style14 {
+            width: 46px;
+            height: 66px;
+        }
+        .auto-style15 {
+            height: 72px;
+            width: 329px;
+        }
+        .auto-style16 {
+            height: 72px;
+            width: 46px;
+        }
+        .auto-style17 {
+            height: 44px;
+        }
+    </style> 
+</head>
+<body>
+    
+    <form id="form1" runat="server">
+        <div>
+            <br />
+         <h2 id ="h2">Unit Converter </h2><br />
+            <br />
+            <table class="auto-style1">
+                <tr>
+                    <td class="auto-style11">Conversion Type :</td>
+                    <td class="auto-style2">
+                        <asp:DropDownList ID="DropDownList1" runat="server" CssClass="auto-style8" Height="29px" Width="165px" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" ViewStateMode="Enabled">
+                            
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style13">Select Units From/To :</td>
+                    <td class="auto-style14">
+                        <asp:DropDownList ID="DropDownList2" runat="server" Height="29px" Width="165px" AutoPostBack="True" >
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style15">Enter the value :</td>
+                    <td class="auto-style16">
+                        <asp:TextBox ID="TextBox1" runat="server" Height="29px" Width="165px"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2" class="auto-style17">
+                        <asp:Button ID="Button1" runat="server" Height="40px" OnClick="Button1_Click" Text="Convert" Width="112px" />
+                     
+                    
+                     <br /> <br/> <asp:Button ID="Button2" runat="server" Text="Clear" Hight="40px" Width="112px" OnClick="btnclear" Height="40px"/></td>
+               
+                   </tr>
+                
+            </table>
+            <br />
+            <br />
+                <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+
+            <br />
+            <br />
+            <asp:TextBox ID="TextBox2" runat="server" TextMode="MultiLine" ReadOnly="True"  AutoPostBack="True" Rows="5" ></asp:TextBox>
+            <br />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment1
+{
+
+    public partial class Default : System.Web.UI.Page
+    {
+        private decimal conversion = 0;
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ArrayList listitem = new ArrayList()
+                {
+                    "Select",
+                    "Length",
+                    "Liquid",
+                    "Temperature",
+                    "Mass"
+                };
+                foreach (object item in listitem)
+                {
+                    DropDownList1.Items.Add(new ListItem(item.ToString(), item.ToString()));
+                }
+                
+                
+            }
+
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            string conversiontype = Convert.ToString(DropDownList1.SelectedValue);
+            string unittype = Convert.ToString(DropDownList2.SelectedValue);
+            double val = Convert.ToInt32(TextBox1.Text);
+            conversion = this.CalculateConversion(conversiontype, unittype, val);
+
+            ArrayList itemList = new ArrayList();
+            {
+                itemList.Add(Label1.Text);
+            }
+            foreach(object item in itemList)
+            {
+                TextBox2.Text += item + "\n";
+
+            }
+                    
+
+
+           
+        }
+
+        
+
+        public decimal CalculateConversion(string conversiontype, string unittype, double val)
+        {
+            double i = int.Parse(TextBox1.Text);
+            if (DropDownList2.SelectedValue == "Kilometers to Miles")
+            {
+                double km = i / 1.609;
+                Label1.Text = int.Parse(TextBox1.Text) + " Kilometers = " + km.ToString() + " Miles";
+            }
+            else if (DropDownList2.SelectedValue == "Miles to Kilometers")
+            {
+                double mile = i * 1.60934;
+                Label1.Text = int.Parse(TextBox1.Text) + " Miles = " + mile.ToString() + " Kilometers";
+            }
+            else if (DropDownList2.SelectedValue == "Yard to Feet")
+            {
+                double yard = i * 3;
+                Label1.Text = int.Parse(TextBox1.Text) + " Yards = " + yard.ToString() + " Feets";
+            }
+            else if (DropDownList2.SelectedValue == "Feet to Yard")
+            {
+                double feets = i / 3;
+                Label1.Text = int.Parse(TextBox1.Text) + " Feets = " + feets.ToString() + " Yards";
+            }
+            else if (DropDownList2.SelectedValue == "Inches to Centimeters")
+            {
+                double inch = i * 2.54;
+                Label1.Text = int.Parse(TextBox1.Text) + " Inches = " + inch.ToString() + " Centimeters";
+            }
+            else if (DropDownList2.SelectedValue == "Centimeters to Inches")
+            {
+                double cm = i / 2.54;
+                Label1.Text = int.Parse(TextBox1.Text) + " Centimeters = " + cm.ToString() + " Inches";
+            }
+            else if (DropDownList2.SelectedValue == "Liters to Gallons")
+            {
+                double Ltr = i / 3.785;
+                Label1.Text = int.Parse(TextBox1.Text) + " Liters = " + Ltr.ToString() + " Gallons";
+            }
+            else if (DropDownList2.SelectedValue == "Gallons to Liters")
+            {
+                double Gl = i * 3.785;
+                Label1.Text = int.Parse(TextBox1.Text) + " Gallons = " + Gl.ToString() + " Liters";
+            }
+            else if (DropDownList2.SelectedValue == "Pints to Gallons")
+            {
+                double pn = i / 8;
+                Label1.Text = int.Parse(TextBox1.Text) + " Pints = " + pn.ToString() + " Gallons";
+            }
+            else if (DropDownList2.SelectedValue == "Gallons to Pints")
+            {
+                double gal = i * 8;
+                Label1.Text = int.Parse(TextBox1.Text) + " Gallons = " + gal.ToString() + " Pints";
+            }
+            else if (DropDownList2.SelectedValue == "Quarts to Gallons")
+            {
+                double qt = i / 4;
+                Label1.Text = int.Parse(TextBox1.Text) + " Quarts = " + qt.ToString() + " Gallons";
+            }
+            else if (DropDownList2.SelectedValue == "Gallons to Quarts")
+            {
+                double Gns = i * 4;
+                Label1.Text = int.Parse(TextBox1.Text) + " Gallons = " + Gns.ToString() + " Quarts";
+            }
+            else if (DropDownList2.SelectedValue == "Fahrenheit to Celsius")
+            {
+                double fh = (i - 32) * 5 / 9;
+                Label1.Text = int.Parse(TextBox1.Text) + " Fahrenheit = " + fh.ToString() + " Cenlsius";
+            }
+            else if (DropDownList2.SelectedValue == "Celsius to Fahrenheit")
+            {
+                double cs = (i * 1.8) + 32;
+                Label1.Text = int.Parse(TextBox1.Text) + " Celsius = " + cs.ToString() + " Fahrenheit";
+            }
+            else if (DropDownList2.SelectedValue == "Kilograms to Pounds")
+            {
+                double kg = i * 2.205;
+                Label1.Text = int.Parse(TextBox1.Text) + " Kilograms = " + kg.ToString() + " Pounds";
+            }
+            else if (DropDownList2.SelectedValue == "Pounds to Kilograms")
+            {
+                double pou = i / 2.205;
+                Label1.Text = int.Parse(TextBox1.Text) + " Pounds = " + pou.ToString() + " Kilograms";
+            }
+            else if (DropDownList2.SelectedValue == "Ounce to Grams")
+            {
+                double oun = i * 28.35;
+                Label1.Text = int.Parse(TextBox1.Text) + " Ounce = " + oun.ToString() + " Grams";
+            }
+            else if (DropDownList2.SelectedValue == "Grams to Ounce")
+            {
+                double Gm = i / 28.35;
+                Label1.Text = int.Parse(TextBox1.Text) + " Grams = " + Gm.ToString() + " Ounce";
+            }
+            else
+            {
+                Label1.Text = "Enter the Valid Input!";
+            }
+            return conversion;
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            string selectedValue = DropDownList1.SelectedItem.Text;
+
+            if (selectedValue == "Length")
+            {
+                ArrayList listitem1 = new ArrayList()
+                {
+                    "Select",
+                   "Kilometers to Miles",
+                   "Miles to Kilometers",
+                   "Yard to Feet",
+                   "Feet to Yard",
+                   "Inches to Centimeters",
+                   "Centimeters to Inches"
+
+                };
+                foreach (object item in listitem1)
+                {
+                    DropDownList2.Items.Add(new ListItem(item.ToString(), item.ToString()));
+                }
+            }
+            if (selectedValue == "Liquid")
+            {
+                ArrayList listitem2 = new ArrayList() {
+                    "Select",
+                    "Liters to Gallons",
+                    "Gallons to Liters",
+                    "Pints to Gallons",
+                    "Gallons to Pints",
+                    "Quarts to Gallons",
+                    "Gallons to Quarts"
+                };
+                foreach (object item in listitem2)
+                {
+                    DropDownList2.Items.Add(new ListItem(item.ToString(), item.ToString()));
+                }
+            }
+            if (selectedValue == "Temperature")
+            {
+
+                ArrayList listitem3 = new ArrayList()
+                {
+                    "Select",
+                    "Fahrenheit to Celsius",
+                    "Celsius to Fahrenheit"
+                };
+                foreach (object item in listitem3)
+                {
+                    DropDownList2.Items.Add(new ListItem(item.ToString(), item.ToString()));
+                }
+            }
+
+            if (selectedValue == "Mass")
+            {
+                ArrayList listitem4 = new ArrayList()
+                {
+                    "Select",
+                    "Kilograms to Pounds",
+                    "Pounds to Kilograms ",
+                    "Ounce to Grams",
+                    "Grams to Ounce"
+                };
+
+                foreach (object item in listitem4)
+                {
+                    DropDownList2.Items.Add(new ListItem(item.ToString(), item.ToString()));
+                }
+
+            }
+        }
+
+        protected void btnclear(object sender, EventArgs e)
+        {
+            DropDownList1.SelectedIndex = 0;
+            DropDownList2.SelectedIndex = 0;
+            TextBox1.Text = "";
+            Label1.Text = "";
+        }
+    }
+}
+ 
